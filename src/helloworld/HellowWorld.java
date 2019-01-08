ae92c7619c77370763df59f6820b2064024aa46a
16872 <16872@DESKTOP-QD1TEI6>
2019-01-08 12:36:58 +0800

    测试

--- src/helloworld/HellowWorld.java
+++ src/helloworld/HellowWorld.java
@@ -2,6 +2,6 @@ package helloworld;
 
 public class HellowWorld {
 	public static void main(String[] args) {
-		System.out.println("Hollow World");
+		System.out.println("Hollow World测试修改git");
 	}
 }
